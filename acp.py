b5a31190a3a57ad21e837296a619ba9cb8728a92
Uzair-dot-rgb <muzair2012@outlook.com>
2026-02-07 10:21:48 -0500

    acp

---
+++ acp.py
@@ -0,0 +1,14 @@
+number_list = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+def multiply_all_numbers(number_list):
+    result = 1
+    for number in number_list:
+        result *= number
+    return result
+print(multiply_all_numbers(number_list))
+number_list2 = (11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+def multiply_all_numbers(number_list2):
+    result = 1
+    for number in number_list2:
+        result *= number
+    return result
+print(multiply_all_numbers(number_list2))
